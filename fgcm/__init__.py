22c51de3d9c0f465ef557419ca6dd3255bdf64f6
Eli Rykoff <erykoff@slac.stanford.edu>
2017-03-02 11:02:31 -0800

    new classes

--- fgcm/__init__.py
+++ fgcm/__init__.py
@@ -3,5 +3,9 @@ from _version import __version__, __version_info__
 version = __version__
 
 from fgcmLUT import FgcmLUT
+from fgcmLUT import FgcmLUTSHM
 from fgcmMakeStars import FgcmMakeStars
+import fgcmUtilities
+from fgcmConfig import FgcmConfig
+from fgcmParameters import FgcmParameters
 
