93c35d2a75dedcd1982d2d42ce29290f4bcae26a
Eli Rykoff <erykoff@slac.stanford.edu>
2017-03-09 12:46:47 -0800

    format exposure table and combine some aux data

--- fgcm/desExposureFormat.py
+++ fgcm/desExposureFormat.py
@@ -15,7 +15,14 @@ class DESExposureFormatter(object):
         self.pmbFile = pmbFile
         self.deltaAperFile = deltaAperFile
 
-    def __run__(self,outFile):
+        if (not os.path.isfile(self.exposureFile)):
+            raise ValueError("Could not find exposureFile: %s" % (self.exposureFile))
+        if (not os.path.isfile(self.pmbFile)):
+            raise ValueError("Could not find pmbFile: %s" % (self.pmbFile))
+        if (not os.path.isfile(self.deltaAperFile)):
+            raise ValueError("Could not find deltaAperFile: %s" % (self.deltaAperFile))
+
+    def __call__(self,outFile,clobber=False):
         expInfoIn = fitsio.read(self.exposureFile,ext=1)
 
         expInfo = np.zeros(expInfoIn.size,dtype=[('EXPNUM','i8'),
@@ -49,14 +56,37 @@ class DESExposureFormatter(object):
             expInfo['TELHA'][i] = sign * 15.0 * (np.abs(float(parts[0])) +
                                                  float(parts[1])/60. +
                                                  float(parts[2])/3600.0)
-            
 
         # and read the pmb file...where did it come from?
+
+        pmbExp = []
+        pmbPressure = []
+        with open(self.pmbFile,'r') as f:
+            line=f.readline()
+            line=f.readline().rstrip('\n').split(',')
+            while line[0] != 'EOF':
+                pmbExp.append(int(line[0]))
+                pmbPressure.append(float(line[1]))
+                line = f.readline().rstrip('\n').split(',')
+
+        pmbExp = np.array(pmbExp,dtype=np.int32)
+        pmbPressure = np.array(pmbPressure,dtype=np.float32)
+
+        expInfo['PMB'] = expInfoIn['PRESSURE'].astype(np.float32)
+        bad,=np.where((~np.isfinite(expInfo['PMB'])) |
+                      (expInfo['PMB'] < 700.0) |
+                      (expInfo['PMB'] > 1000.0))
+        a,b=esutil.numpy_util.match(expInfo['EXPNUM'][bad],pmbExp)
+
+        if (a.size < bad.size):
+            raise ValueError("There are %d missing pressure values!" % (bad.size-a.size))
+
+        expInfo['PMB'][bad[a]] = pmbPressure[b]
 
         # and read the aperfile
         deltaAper=fitsio.read(self.deltaAperFile,ext=1)
 
         a,b=esutil.numpy_util.match(deltaAper['EXPNUM'],expInfo['EXPNUM'])
-        expInfo['DELTA_APER'][b] = deltaAper['DMAG7M9'][a]
+        expInfo['DELTA_APER'][b] = deltaAper['APER7M9'][a]
 
-        fitsio.write(outfile,expInfo)
+        fitsio.write(outFile,expInfo,clobber=clobber)
