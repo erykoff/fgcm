b7b344385838530bea1f21aeff52c90abf26770b
Eli Rykoff <erykoff@slac.stanford.edu>
2017-03-22 09:57:04 -0700

    now with working chi^2 computation!

--- fgcm/fgcmChisq.py
+++ fgcm/fgcmChisq.py
@@ -39,6 +39,11 @@ class FgcmChisq(object):
         # also shm'd
         self.fgcmStars = fgcmStars
 
+        # need to configure
+        self.nCore = 4
+
+        self.fitChisqs = []
+
         # not sure what we need the config for
 
         resourceUsage('End of chisq init')
@@ -83,19 +88,60 @@ class FgcmChisq(object):
         goodStars,=np.where(snmm.getArray(self.fgcmStars.starFlagHandle) == 0)
 
         # testing
-        goodStars=goodStars[0:10]
+        #goodStars=goodStars[0:1000]
+
+        # prepare the return arrays...
+        # how many do we have?
+
+        self.nSums = 2   # chisq, nobs
+        if (self.computeDerivatives):
+            self.nSums += self.fgcmPars.nFitPars  # one for each parameter
+
+        # make a dummy process to discover starting child number
+        proc = multiprocessing.Process()
+        workerIndex = proc._identity[0]+1
+        proc = None
+
+        self.totalHandleDict = {}
+        for thisCore in xrange(self.nCore):
+            self.totalHandleDict[workerIndex + thisCore] = snmm.createArray(self.nSums,dtype='f4')
+
 
         # will want to make a pool
 
         pool = Pool(processes=4)
         resourceUsage('premap')
         pool.map(self._worker,goodStars)
+        pool.close()
+        pool.join()
+
+        # and return the derivatives + chisq
+        partialSums = np.zeros(self.nSums,dtype='f8')
+        for thisCore in xrange(self.nCore):
+            partialSums[:] += snmm.getArray(self.totalHandleDict[workerIndex + thisCore])[:]
+
+        fitDOF = partialSums[-1] - float(self.fgcmPars.nFitPars)
+        if (fitDOF <= 0):
+            raise ValueError("Number of parameters fitted is more than number of constraints! (%d > %d)" % (self.fgcmPars.nFitPars,partialSums[-1]))
+
+        fitChisq = partialSums[-2] / fitDOF
+        if (self.computeDerivatives):
+            dChisqdP = partialSums[0:self.fgcmPars.nFitPars] / fitDOF
+
+        # want to append this...
+        self.fitChisqs.append(fitChisq)
 
         # free shared arrays
-        #snmm.freeArray(self.obsExpFlagHandle)
         snmm.freeArray(self.obsExpIndexHandle)
+        for key in self.totalHandleDict.keys():
+            snmm.freeArray(self.totalHandleDict[key])
 
         resourceUsage('end')
+
+        if (self.computeDerivatives):
+            return fitChisq, dChisqdP
+        else:
+            return fitChisq
 
     def _worker(self,objIndex):
         """
@@ -111,15 +157,20 @@ class FgcmChisq(object):
         objNobs = snmm.getArray(self.fgcmStars.objNobsHandle)
 
         thisObsIndex = obsIndex[objObsIndex[objIndex]:objObsIndex[objIndex]+objNobs[objIndex]]
+        thisObsExpIndex = snmm.getArray(self.obsExpIndexHandle)[thisObsIndex]
+
+        # cut to good exposures
+        #  I think this can be done in the parent more efficiently...but not now.
+        gd,=np.where(self.fgcmPars.expFlag[thisObsExpIndex] == 0)
+        thisObsIndex=thisObsIndex[gd]
+        thisObsExpIndex = thisObsExpIndex[gd]
+
         thisObsBandIndex = snmm.getArray(self.fgcmStars.obsBandIndexHandle)[thisObsIndex]
         thisObsCCDIndex = snmm.getArray(self.fgcmStars.obsCCDHandle)[thisObsIndex] - 1
 
         obsMagADU = snmm.getArray(self.fgcmStars.obsMagADUHandle)
         obsMagADUErr = snmm.getArray(self.fgcmStars.obsMagADUErrHandle)
         obsMagStd = snmm.getArray(self.fgcmStars.obsMagStdHandle)
-
-        #goodExpIndex = snmm.getArray(self.goodExpIndexHandle)
-        thisObsExpIndex = snmm.getArray(self.obsExpIndexHandle)[thisObsIndex]
 
         # these IDs check out!
         #print(snmm.getArray(self.fgcmStars.obsObjIDHandle)[thisObsIndex])
@@ -136,8 +187,8 @@ class FgcmChisq(object):
         thisSecZenith = 1./(np.sin(thisObjDec)*self.fgcmPars.sinLatitude +
                         np.cos(thisObjDec)*self.fgcmPars.cosLatitude*np.cos(thisObjHA))
 
-        print(thisObsBandIndex)
-        print(thisSecZenith)
+        #print(thisObsBandIndex)
+        #print(thisSecZenith)
 
         # get I0obs values...
         lutIndices = self.fgcmLUT.getIndices(thisObsBandIndex,
@@ -149,13 +200,13 @@ class FgcmChisq(object):
                                              thisObsCCDIndex,
                                              self.fgcmPars.expPmb[thisObsExpIndex])
 
-        print("exp:",self.fgcmPars.expArray[thisObsExpIndex])
-        print("pwv:",self.fgcmPars.expPWV[thisObsExpIndex])
-        print("o3:",self.fgcmPars.expO3[thisObsExpIndex])
-        print("tau:",self.fgcmPars.expTau[thisObsExpIndex])
-        print("alpha:",self.fgcmPars.expAlpha[thisObsExpIndex])
-        print("ccd:",thisObsCCDIndex)
-        print("pmb:",self.fgcmPars.expPmb[thisObsExpIndex])
+        #print("exp:",self.fgcmPars.expArray[thisObsExpIndex])
+        #print("pwv:",self.fgcmPars.expPWV[thisObsExpIndex])
+        #print("o3:",self.fgcmPars.expO3[thisObsExpIndex])
+        #print("tau:",self.fgcmPars.expTau[thisObsExpIndex])
+        #print("alpha:",self.fgcmPars.expAlpha[thisObsExpIndex])
+        #print("ccd:",thisObsCCDIndex)
+        #print("pmb:",self.fgcmPars.expPmb[thisObsExpIndex])
 
         # and I10obs values...
         thisI0 = self.fgcmLUT.computeI0(thisObsBandIndex,
@@ -169,8 +220,8 @@ class FgcmChisq(object):
                                         lutIndices)
         thisI10 = self.fgcmLUT.computeI1(lutIndices) / thisI0
 
-        print("I0:",thisI0)
-        print("I10:",thisI10)
+        #print("I0:",thisI0)
+        #print("I10:",thisI10)
 
         thisQESys = self.fgcmPars.expQESys[thisObsExpIndex]
 
@@ -188,8 +239,9 @@ class FgcmChisq(object):
             for i in xrange(self.fgcmStars.nBands):
                 #use,=np.where((obsExpFlag[thisObsIndex] == 0) &
                 #              (obsBandIndex[thisObsIndex] == i))
-                use,=np.where((self.fgcmPars.expFlag[thisObsExpIndex] == 0) &
-                              (thisObsBandIndex == i))
+                #use,=np.where((self.fgcmPars.expFlag[thisObsExpIndex] == 0) &
+                #              (thisObsBandIndex == i))
+                use,=np.where(thisObsBandIndex == i)
                 if (use.size > 0):
                     wtSum = np.sum(1./thisMagErr2[use])
                     objMagStdMeanErr[objIndex,i] = np.sqrt(1./wtSum)
@@ -201,6 +253,7 @@ class FgcmChisq(object):
                 objSEDSlope[objIndex,:] = 0.0
             else:
                 # need to do FIT BANDS
+                #   FIXME
                 S = np.zeros(self.fgcmPars.nBands-1,dtype='f4')
                 for i in xrange(self.fgcmPars.nBands-1):
                     S[i] = -0.921 * (objMagStdMean[objIndex,i+1] - objMagStdMean[objIndex,i])/(self.fgcmLUT.lambdaStd[i+1] - self.fgcmLUT.lambdaStd[i])
@@ -222,14 +275,37 @@ class FgcmChisq(object):
         for i in xrange(self.fgcmStars.nBands):
             #use,=np.where((obsExpFlag[thisObsIndex] == 0) &
             #              (thisObsBandIndex == i))
-            use,=np.where((self.fgcmPars.expFlag[thisObsExpIndex] == 0) &
-                          (thisObsBandIndex == i))
+            #use,=np.where((self.fgcmPars.expFlag[thisObsExpIndex] == 0) &
+            #              (thisObsBandIndex == i))
+            use,=np.where(thisObsBandIndex == i)
             if (use.size > 0):
                 wtSum = np.sum(1./thisMagErr2[use])
                 objMagStdMeanErr[objIndex,i] = np.sqrt(1./wtSum)
                 objMagStdMean[objIndex,i] = np.sum(obsMagStd[thisObsIndex[use]]/
                                                    thisMagErr2[use])/wtSum
 
+        # compute deltaMag
+        deltaMag = obsMagStd[thisObsIndex] - objMagStdMean[objIndex,thisObsBandIndex]
+        deltaMagErr2 = thisMagErr2 + objMagStdMeanErr[objIndex,thisObsBandIndex]**2.
 
+        # finally, compute the chisq.  Also need a return array!
+        partialChisq = np.sum(deltaMag**2./deltaMagErr2)
+
+        partialArray = np.zeros(self.nSums,dtype='f4')
+
+        # last one is the chisq
+        partialArray[-2] = partialChisq
+        partialArray[-1] = thisObsIndex.size
+
+        # and compute the derivatives if desired...
         if (self.computeDerivatives):
             pass
+
+        # note that this doesn't need locking because we are only accessing
+        #   a single array from a single process
+        thisCore = multiprocessing.current_process()._identity[0]
+        totalArr = snmm.getArray(self.totalHandleDict[thisCore])
+        totalArr[:] = totalArr[:] + partialArray
+
+        # no return
+        return None
