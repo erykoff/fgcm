5c7433e97ec1bfeb197af081129dc10e71c19f2c
Eli Rykoff <erykoff@slac.stanford.edu>
2017-03-27 10:11:05 -0700

    now will record number of observations per band.

--- fgcm/fgcmStars.py
+++ fgcm/fgcmStars.py
@@ -15,7 +15,7 @@ from sharedNumpyMemManager import SharedNumpyMemManager as snmm
 copy_reg.pickle(types.MethodType, _pickle_method)
 
 class FgcmStars(object):
-    def __init__(self,fgcmConfig):
+    def __init__(self,fgcmConfig,computeNobs=True):
         self.obsFile = fgcmConfig.obsFile
         self.indexFile = fgcmConfig.indexFile
 
@@ -32,6 +32,10 @@ class FgcmStars(object):
                 self.bandRequired[i] = True
 
         self._loadStars()
+
+        if (computeNobs):
+            allExp = np.arange(fgcmConfig.expRange[0],fgcmConfig.expRange[1],dtype='i4')
+            self.selectStarsMinObs(allExp)
 
     def _loadStars(self):
 
@@ -54,15 +58,6 @@ class FgcmStars(object):
         # need to stuff into shared memory objects.
         self.nStarObs = obs.size
 
-        #self.obsExp = shm.zeros(self.nStarObs,dtype='i4')
-        #self.obsCCD = shm.zeros(self.nStarObs,dtype='i2')
-        #self.obsBandIndex = shm.zeros(self.nStarObs,dtype='i2')
-        #self.obsRA = shm.zeros(self.nStarObs,dtype='f8')
-        #self.obsDec = shm.zeros(self.nStarObs,dtype='f8')
-        #self.obsMagADU = shm.zeros(self.nStarObs,dtype='f4')
-        #self.obsMagADUErr = shm.zeros(self.nStarObs,dtype='f4')
-        #self.obsMagStd = shm.zeros(self.nStarObs,dtype='f4')
-
         self.obsExpHandle = snmm.createArray(self.nStarObs,dtype='i4')
         self.obsCCDHandle = snmm.createArray(self.nStarObs,dtype='i2')
         self.obsBandIndexHandle = snmm.createArray(self.nStarObs,dtype='i2')
@@ -71,14 +66,6 @@ class FgcmStars(object):
         self.obsMagADUHandle = snmm.createArray(self.nStarObs,dtype='f4')
         self.obsMagADUErrHandle = snmm.createArray(self.nStarObs,dtype='f4')
         self.obsMagStdHandle = snmm.createArray(self.nStarObs,dtype='f4')
-
-        #self.obsExp[:] = obs['EXPNUM'][:]
-        #self.obsCCD[:] = obs['CCDNUM'][:]
-        #self.obsRA[:] = obs['RA'][:]
-        #self.obsDec[:] = obs['DEC'][:]
-        #self.obsMagADU[:] = obs['MAG'][:]
-        #self.obsMagADUErr[:] = obs['MAGERR'][:]
-        #self.obsMagStd[:] = obs['MAG'][:]  # default
 
         snmm.getArray(self.obsExpHandle)[:] = obs['EXPNUM'][:]
         snmm.getArray(self.obsCCDHandle)[:] = obs['CCDNUM'][:]
@@ -102,23 +89,12 @@ class FgcmStars(object):
 
         self.nStars = pos.size
 
-        #self.objID = shm.zeros(self.nStars,dtype='i4')
-        #self.objRA = shm.zeros(self.nStars,dtype='f8')
-        #self.objDec = shm.zeros(self.nStars,dtype='f8')
-        #self.objObsIndex = shm.zeros(self.nStars,dtype='i4')
-        #self.objNobs = shm.zeros(self.nStars,dtype='i4')
-
         self.objIDHandle = snmm.createArray(self.nStars,dtype='i4')
         self.objRAHandle = snmm.createArray(self.nStars,dtype='f8')
         self.objDecHandle = snmm.createArray(self.nStars,dtype='f8')
         self.objObsIndexHandle = snmm.createArray(self.nStars,dtype='i4')
         self.objNobsHandle = snmm.createArray(self.nStars,dtype='i4')
-
-        #self.objID[:] = pos['FGCM_ID'][:]
-        #self.objRA[:] = pos['RA'][:]
-        #self.objDec[:] = pos['DEC'][:]
-        #self.objObsIndex[:] = pos['OBSINDEX'][:]
-        #self.objNobs[:] = pos['NOBS'][:]
+        self.objNGoodObsHandle = snmm.createArray((self.nStars,self.nBands),dtype='i4')
 
         snmm.getArray(self.objIDHandle)[:] = pos['FGCM_ID'][:]
         snmm.getArray(self.objRAHandle)[:] = pos['RA'][:]
@@ -129,11 +105,6 @@ class FgcmStars(object):
 
         self.minObjID = np.min(snmm.getArray(self.objIDHandle))
         self.maxObjID = np.max(snmm.getArray(self.objIDHandle))
-
-        # and create reverse look-up ids
-        #self.obsObjID = shm.zeros(self.nStarObs,dtype='i4')
-        #for i in xrange(self.nStars):
-        #    self.obsObjID[self.obsIndex[self.objObsIndex[i]:self.objObsIndex[i]+self.objNobs[i]]] = self.objID[i]
 
         self.obsObjIDHandle = snmm.createArray(self.nStarObs,dtype='i4')
         obsObjID = snmm.getArray(self.obsObjIDHandle)
@@ -156,54 +127,54 @@ class FgcmStars(object):
         # 1: bad...
         # 2: ???
 
-        #self.starFlag = shm.zeros(self.nStars,dtype=np.int16)
         self.starFlagHandle = snmm.createArray(self.nStars,dtype='i2')
 
         # And we need to record the mean mag, error, SED slopes...
-
-        #self.objMagStdMean = shm.zeros((self.nStars,self.nBands),dtype='f4')
-        #self.objMagStdMeanErr = shm.zeros((self.nStars,self.nBands),dtype='f4')
-        #self.objSEDSlope = shm.zeros((self.nStars,self.nBands),dtype='f4')
 
         self.objMagStdMeanHandle = snmm.createArray((self.nStars,self.nBands),dtype='f4')
         self.objMagStdMeanErrHandle = snmm.createArray((self.nStars,self.nBands),dtype='f4')
         self.objSEDSlopeHandle = snmm.createArray((self.nStars,self.nBands),dtype='f4')
 
-    def selectStars(self,goodExps):
+    def selectStarsMinObs(self,goodExps):
         """
         """
 
-        # want to select out stars that are seen on the good exposures
-        # and we need to know the number of observations in each...
-
-        # some testing...
-        test=fitsio.read('/nfs/slac/g/ki/ki19/des/erykoff/des/y3a1/calibration_testing/fgcm_refactor_tests/pars1/y3a1_exposure_info.fits',ext=1)
-        goodExps = test['EXPNUM']
+        # Given a list of good exposures, which stars have at least minObs observations
+        #  in each required band?
 
         obsExp = snmm.getArray(self.obsExpHandle)
         obsIndex = snmm.getArray(self.obsIndexHandle)
+        obsBandIndex = snmm.getArray(self.obsBandIndexHandle)
         obsObjID = snmm.getArray(self.obsObjIDHandle)
+        objNGoodObs = snmm.getArray(self.objNGoodObsHandle)
+        objID = snmm.getArray(self.objIDHandle)
 
         a,b=esutil.numpy_util.match(goodExps,obsExp[obsIndex])
 
         req,=np.where(self.bandRequired)
 
-        nobs = np.zeros((req.size, self.nStars),dtype='i4')
-        for i in xrange(req.size):
-            use,=np.where(self.obsBandIndex[obsIndex[b]] == req[i])
-            hist=esutil.stat.histogram(obsObjID[obsIndex[b[use]]],
-                                       min=self.minObjID,max=self.maxObjID)
-            nobs[i,:] = hist[self.objID - self.minObjID]
+        #nobs = np.zeros((req.size, self.nStars),dtype='i4')
+        #for i in xrange(req.size):
+        #    use,=np.where(obsBandIndex[obsIndex[b]] == req[i])
+        #    hist=esutil.stat.histogram(obsObjID[obsIndex[b[use]]],
+        #                               min=self.minObjID,max=self.maxObjID)
+            #nobs[i,:] = hist[self.objID - self.minObjID]
+        #    objNGoodObs[:,i] = hist[objID - self.minObjID]
 
-        minObs = nobs.min(axis=0)
+        # Better indexed version
+        objNGoodObs[:,:] = 0.0
+        tempObjID = np.arange(self.minObjID,self.maxObjID+1,dtype='i8')
+        tempObjIndex = np.searchsorted(objID,tempObjID)
+        np.add.at(objNGoodObs,
+                  (tempObjIndex[obsObjID[obsIndex[b]] - self.minObjID],
+                   obsBandIndex[obsIndex[b]]),
+                  1)
+
+        minObs = objNGoodObs[:,req].min(axis=1)
 
         snmm.getArray(self.starFlagHandle)[:] = 0
-        #self.starFlag[:] = 0
         bad,=np.where(minObs < self.minPerBand)
-        #self.starFlag[bad] = 1
         snmm.getArray(self.starFlagHandle)[bad] = 1
-
-        # and further selection?
 
     def computeObjectSEDSlope(self,objIndex):
         """
