764acca63de50705b2cece6fd9a755e9f634401b
Eli Rykoff <erykoff@slac.stanford.edu>
2017-04-11 13:30:37 -0700

    dataBinner added

--- fgcm/fgcmUtilities.py
+++ fgcm/fgcmUtilities.py
@@ -21,3 +21,53 @@ def resourceUsage(where):
         print('Could not find self status.')
     return
 
+def dataBinner(x,y,binSize,xRange,nTrial=100,xNorm=-1.0):
+    """
+    """
+
+
+    import esutil
+
+    hist,rev=esutil.stat.histogram(x,binsize=binsize,min=xRange[0],max=xRange[1]-0.0001,rev=True)
+    binStruct=np.zeros(hist.size,dtype=[('X_BIN','f4'),
+                                        ('X','f4'),
+                                        ('X_ERR_MEAN','f4'),
+                                        ('X_ERR','f4'),
+                                        ('Y','f4'),
+                                        ('Y_WIDTH','f4'),
+                                        ('Y_ERR','f4'),
+                                        ('N','i4')])
+    binStruct['X_BIN'] = np.linspace(xRange[0],xRange[1],hist.size)
+
+    for i in xrange(hist.size):
+        if (hist[i] >= 10):
+            i1a=rev[rev[i]:rev[i+1]]
+
+            binStruct['N'][i] = i1a.size
+
+            medYs=np.zeros(ntrial,dtype='f8')
+            medYWidths=np.zeros(ntrial,dtype='f8')
+            medXs=np.zeros(ntrial,dtype='f8')
+            medXWidths=np.zeros(ntrial,dtype='f8')
+
+            for t in xrange(nTrial):
+                r=(np.random.random(i1a.size)*i1a.size).astype('i4')
+
+                medYs[t] = np.median(y[i1a[r]])
+                medYWidths[t] = 1.4826*np.median(np.abs(y[i1a[r]] - medYs[t]))
+
+                medXs[t] = np.median(x[i1a[r]])
+                medXWidths[t] = 1.4826*np.median(np.abs(x[i1a[r]] - medXs[t]))
+
+            binStruct['X'][i] = np.median(medXs)
+            binStruct['X_ERR'][i] = np.median(medXWidths)
+            binStruct['X_ERR_MEAN'][i] = 1.4826*np.median(np.abs(medXs - binStruct['X'][i]))
+            binStruct['Y'][i] = np.median(medYs)
+            binStruct['Y_WIDTH'][i] = np.median(medYWidths)
+            binStruct['Y_ERR'][i] = 1.4826*np.median(np.abs(medYs - binStruct['Y'][i]))
+
+    if (xNorm >= 0.0) :
+        ind=np.clip(np.searchsorted(binStruct['X_BIN'],xnorm),0,binStruct.size-1)
+        binStruct['Y'] = binStruct['Y'] - binStruct['Y'][ind]
+
+    return binStruct
