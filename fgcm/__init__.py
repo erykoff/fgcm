d4741349ae8e801fd675873393024d47107b2fe0
Eli Rykoff <erykoff@slac.stanford.edu>
2017-04-11 13:29:13 -0700

    aperture correction

--- fgcm/__init__.py
+++ fgcm/__init__.py
@@ -13,6 +13,7 @@ from fgcmChisq import FgcmChisq
 from fgcmBrightObs import FgcmBrightObs
 from fgcmGray import FgcmGray
 from fgcmSuperStarFlat import FgcmSuperStarFlat
+from fgcmApertureCorrection import FgcmApertureCorrection
 from desGPSFormat import DESGPSFormatter
 from desExposureFormat import DESExposureFormatter
 
