8107499dff319c2176e787ad140d584c7ae2dd5f
Eli Rykoff <erykoff@slac.stanford.edu>
2017-04-20 14:21:48 -0700

    command line front-end to fit cycle.

---
+++ scripts/runFgcmFitCycle.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import os
+import sys
+import argparse
+import fgcm
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(decription='Code to run FGCM fit cycle')
+
+    parser.add_argument('-c','--config', action='store', type='str', required=True,
+                        help='YAML config file')
+
+    args = parser.parse_args()
+
+    fgcmFitCycle = fgcm.FgcmFitCycle(args.config)
+
+    fgcmFitCycle.run()
+
