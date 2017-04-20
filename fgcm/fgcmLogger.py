8fd85e86728a014c6b40cacb34355284b0be5a04
Eli Rykoff <erykoff@slac.stanford.edu>
2017-04-20 06:55:54 -0700

    lightweight logger

---
+++ fgcm/fgcmLogger.py
@@ -0,0 +1,33 @@
+from __future__ import print_function
+
+import os
+import sys
+
+from fgcmUtilities import logDict
+
+class FgcmLogger(object):
+    """
+    """
+    def __init__(self,logFile,logLevel):
+
+        self.logFile = logFile
+
+        # this might fail.  Let it throw its exception?
+        self.logF = open(self.logFile,'w')
+
+        self.logLevel = logLevel
+
+        if (logLevel not in logDict):
+            raise ValueError("Illegal logLevel: %s" % (logLevel))
+
+
+    def log(self,logType,logString):
+
+        if (logDict[logType] <= logDict[self.logLevel]):
+            f.write(logString)
+            print(logString)
+
+    def stopLogging(self):
+        self.logF.close()
+
+        self.logLevel == 0
