bd1f0b819868e5fbe36c4e2ed7873d3bb4ac9019
Eli Rykoff <erykoff@slac.stanford.edu>
2017-10-30 09:27:19 -0700

    add clobber and printonly options

--- scripts/runFgcmFitCycle.py
+++ scripts/runFgcmFitCycle.py
@@ -13,17 +13,18 @@ if __name__ == '__main__':
 
     parser.add_argument('-c','--config', action='store', type=str, required=True,
                         help='YAML config file')
+    parser.add_argument('-C','--clobber', action='store_true', default=False, help='Clobber existing run')
+    parser.add_argument('-p','--printOnly', action='store_true', default=False, help='Only print logging to stdout')
 
     args = parser.parse_args()
-
-    #fgcmFitCycle = fgcm.FgcmFitCycle(args.config)
-    #fgcmFitCycle = fgcm.FgcmFitCycleNew(args.config)
-    #fgcmFitCycle.run()
 
     with open(args.config) as f:
         configDict = yaml.load(f)
 
     print("Configuration read from %s" % (args.config))
+
+    configDict['clobber'] = args.clobber
+    configDict['printOnly'] = args.printOnly
 
     fgcmFitCycle = fgcm.FgcmFitCycle(configDict, useFits=True)
     fgcmFitCycle.runWithFits()
