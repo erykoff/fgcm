4ae23c1988785443828b40005ab9bb89ed1b52ed
Eli Rykoff <erykoff@slac.stanford.edu>
2017-12-19 07:30:05 -0800

    Script to build an atmosphere file

---
+++ scripts/makeFgcmAtmosphereTable.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+from __future__ import print_function
+
+import os
+import sys
+import argparse
+import fgcm
+import yaml
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Code to make FGCM modtran atmosphere tables')
+
+    parser.add_argument('-c','--config', action='store', type=str, required=True,
+                        help='YAML config file')
+    parser.add_argument('-C','--clobber', action='store', type=bool, required=False,
+                        default=False,help='Clobber existing LUT?')
+
+
+    args = parser.parse_args()
+
+    with open(args.config,'r') as f:
+        lutConfig = yaml.load(f)
+
+    if 'atmosphereTableFile' not in lutConfig:
+        raise ValueError("Must include atmosphereTableFile in config to run makeFgcmAtmosphereTable.py")
+
+    if ((not args.clobber) and os.path.isfile(lutConfig['atmosphereTableFile'])):
+        print("atmosphereTableFile %s already found, and clobber set to False." % (lutConfig['atmosphereTableFile']))
+        sys.exit(0)
+
+    fgcmAtmosphereTable = fgcm.FgcmAtmosphereTable(lutConfig)
+    fgcmAtmosphereTable.generateTable()
+    fgcmAtmosphereTable.saveTable(lutConfig['atmosphereTableFile'])
+
+
+
