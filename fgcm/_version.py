6619c9fa13e056d721ec62f9b4c1b5cf010b1ece
Eli Rykoff <erykoff@stanford.edu>
2018-03-26 13:23:27 -0700

    1.1.0

--- fgcm/_version.py
+++ fgcm/_version.py
@@ -1,5 +1,5 @@
 from __future__ import division, absolute_import, print_function
 
-__version__ = '1.0.3'
+__version__ = '1.1.0'
 #__version_info__ = tuple(map(int, __version__.split('.')))
 __version_info__ = __version__.split('.')
