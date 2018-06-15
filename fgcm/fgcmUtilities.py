Maybe fix the ccd rotation plotting bug?

fgcm/fgcmUtilities.py
@@ -424,14 +424,19 @@ def plotCCDMapPoly2d(ax, ccdOffsets, parArray, cbLabel, loHi=None):
                       ccdOffsets['DELTA_DEC'][k] +
                       ccdOffsets['DECSIGN'][k]*ccdOffsets['DEC_SIZE'][k]/2.]
         """
-        # FIXME: probably for a rotated camera, we need to transpose the zGrid?
-        # To be confirmed
-
-        plt.imshow(zGrid.reshape(xValues.size, yValues.size),
-                   interpolation='bilinear',
-                   origin='lower',
-                   extent=extent,
-                   norm=cNorm)
+        if ccdOffsets['xra'][k]:
+            # swap x/y axes
+            plt.imshow(zGrid.reshape(yValues.size, xValues.size),
+                       interpolation='bilinear',
+                       origin='lower',
+                       extent=extent,
+                       norm=cNorm)
+        else:
+            plt.imshow(zGrid.reshape(xValues.size, yValues.size),
+                       interpolation='bilinear',
+                       origin='lower',
+                       extent=extent,
+                       norm=cNorm)
 
     cb=None
     cb = plt.colorbar(CS3,ticks=np.linspace(lo,hi,5))