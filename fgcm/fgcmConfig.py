b4c8bd8525970b9d6d8245a353b4a21950870c33
Eli Rykoff <erykoff@stanford.edu>
2019-02-15 07:10:53 -0800

    Add configuration to apply color cuts to reference stars

--- fgcm/fgcmConfig.py
+++ fgcm/fgcmConfig.py
@@ -162,6 +162,7 @@ class FgcmConfig(object):
 
     refStarSnMin = ConfigField(float, default=20.0)
     refStarOutlierNSig = ConfigField(float, default=4.0)
+    applyRefStarColorCuts = ConfigField(bool, default=True)
 
     mapNSide = ConfigField(int, default=256)
     nStarPerRun = ConfigField(int, default=200000)
