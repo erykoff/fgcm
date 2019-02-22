858dde7fbe94df614673e1ff73acc9bf07f9721e
Eli Rykoff <erykoff@stanford.edu>
2019-02-22 10:17:05 -0800

    Option to use reference stars with instrument fitting.

--- fgcm/fgcmConfig.py
+++ fgcm/fgcmConfig.py
@@ -163,6 +163,7 @@ class FgcmConfig(object):
     refStarSnMin = ConfigField(float, default=20.0)
     refStarOutlierNSig = ConfigField(float, default=4.0)
     applyRefStarColorCuts = ConfigField(bool, default=True)
+    useRefStarsWithInstrument = ConfigField(bool, default=False)
 
     mapNSide = ConfigField(int, default=256)
     nStarPerRun = ConfigField(int, default=200000)
